06f2a78b76d7a871bd7eb12e26478fafea532563
216749 Krystian Domdalski <216749@edu.p.lodz.pl>
2020-05-15 01:42:32 +0200

    A few tweaks

--- frontend/src/components/registration/Registration.js
+++ frontend/src/components/registration/Registration.js
@@ -1,192 +1,208 @@
-import React, {useState} from 'react';
-import 'antd/dist/antd.css';
-import './Registration.css';
-import {Button, Form, Input, notification} from 'antd';
-import {checkEmailAvailability, checkUsernameAvailability, register} from "../../utils/Requests";
-import history from "../../history";
+import React, {useState} from 'react'
+import 'antd/dist/antd.css'
+import './Registration.css'
+import {Button, Form, Input, notification} from 'antd'
+import {checkEmailAvailability, checkUsernameAvailability, register} from "../../utils/Requests"
+import history from "../../history"
 
 
 const RegistrationForm = () => {
-        const [form] = Form.useForm();
-        const [usernameValidationStatus, setUsernameValidationStatus] = useState('');
-        const [usernameErrorMsg, setUsernameErrorMsg] = useState(null);
-        const [emailValidationStatus, setEmailValidationStatus] = useState(null);
-        const [emailErrorMsg, setEmailErrorMsg] = useState(null);
-        const [username, setUsername] = useState('');
-        const [email, setEmail] = useState('');
+    const [form] = Form.useForm();
+    const [usernameValidationStatus, setUsernameValidationStatus] = useState('')
+    const [usernameErrorMsg, setUsernameErrorMsg] = useState(null)
+    const [emailValidationStatus, setEmailValidationStatus] = useState('')
+    const [emailErrorMsg, setEmailErrorMsg] = useState(null)
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const {getFieldError} = form
 
-        const onFinish = values => {
-            const registrationRequest = Object.assign({}, values);
-            delete registrationRequest.confirm
-            register(registrationRequest)
-                .then(response => {
-                    notification.success({
-                        message: 'App',
-                        description: "You've successfully registered. You may log in now."
-                    });
-                    history.push('/login');
-                }).catch(error => {
-                notification.error({
+    const onFinish = values => {
+        const registrationRequest = Object.assign({}, values)
+        delete registrationRequest.confirm
+        register(registrationRequest)
+            .then(() => {
+                notification.success({
                     message: 'App',
-                    description: error.message || 'Sorry. Something went wrong. Please try again.'
+                    description: "You've successfully registered. You may log in now."
                 });
-            });
-        }
+                history.push('/login')
+            }).catch(error => {
+            notification.error({
+                message: 'App',
+                description: error.message || 'Sorry. Something went wrong. Please try again.'
+            })
+        })
+    }
 
-        function validateUsernameAvailability() {
+    const validateUsernameAvailability = () => {
+        if (getFieldError('username').length === 0) {
             checkUsernameAvailability(username)
                 .then(response => {
                     if (response.available) {
                         setUsernameValidationStatus('success');
-                        setUsernameErrorMsg(null);
+                        setUsernameErrorMsg(null)
                     } else {
                         setUsernameValidationStatus('error');
                         setUsernameErrorMsg('Username is already taken');
                     }
-                });
+                })
         }
+    }
 
-        function validateEmailAvailability() {
+    const validateEmailAvailability = () => {
+        if (getFieldError('email').length === 0) {
             checkEmailAvailability(email)
                 .then(response => {
                     if (response.available) {
-                        setEmailValidationStatus('success');
-                        setEmailErrorMsg(null);
+                        setEmailValidationStatus('success')
+                        setEmailErrorMsg(null)
                     } else {
-                        setEmailValidationStatus('error');
-                        setEmailErrorMsg('E-mail address is already taken');
+                        setEmailValidationStatus('error')
+                        setEmailErrorMsg('E-mail address is already taken')
                     }
-                });
+                })
         }
-
-        function validateUsername() {
-
-        }
-
-        return (
-            <Form
-                form={form}
-                layout={"vertical"}
-                name={"registration_form"}
-                className={"registration-form"}
-                onFinish={onFinish}
-                scrollToFirstError
-            >
-                <Form.Item
-                    label={"Username"}
-                    validateStatus={usernameValidationStatus}
-                    help={usernameErrorMsg}
-                    name={"username"}
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Username is required'
-                        },
-                    ]}
-                >
-                    <Input
-                        value={username}
-                        onChange={event => setUsername(event.target.value)}
-                        onBlur={validateUsernameAvailability}/>
-                </Form.Item>
-
-                <Form.Item
-                    label={"First Name"}
-                    name={"firstName"}
-                    rules={[
-                        {
-                            required: true,
-                            message: 'First Name is required'
-                        }
-                    ]}
-                >
-                    <Input/>
-                </Form.Item>
-
-                <Form.Item
-                    label={"Last Name"}
-                    name={"lastName"}
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Last Name is required'
-                        }
-                    ]}
-                >
-                    <Input/>
-                </Form.Item>
-
-                <Form.Item
-                    label={"E-mail"}
-                    validateStatus={emailValidationStatus}
-                    help={emailErrorMsg}
-                    name={"email"}
-                    rules={[
-                        {
-                            type: 'email',
-                            message: 'Invalid E-mail address',
-
-                        },
-                        {
-                            required: true,
-                            message: 'E-mail address is required'
-                        }
-                    ]}
-                >
-
-                    <Input
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                        onBlur={validateEmailAvailability}/>
-                </Form.Item>
-
-                <Form.Item
-                    label={"Password"}
-                    name={"password"}
-                    hasFeedback
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Password is required'
-                        }
-                    ]}
-                >
-                    <Input.Password/>
-                </Form.Item>
-
-                <Form.Item
-                    label={"Confirm Password"}
-                    name={"confirm"}
-                    dependencies={['password']}
-                    hasFeedback
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Password confirmation is required'
-                        },
-                        ({getFieldValue}) => ({
-                            validator(rule, value) {
-                                if (!value || getFieldValue('password') === value) {
-                                    return Promise.resolve();
-                                }
-
-                                return Promise.reject('Passwords do not match');
-                            }
-                        })
-                    ]}
-                >
-                    <Input.Password/>
-                </Form.Item>
-
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="registration-form-button">
-                        Register
-                    </Button>
-                    <text className="login-link">Already have an acoount? <a href="../login">Login</a></text>
-                </Form.Item>
-            </Form>
-        );
     }
-;
+
+    return (
+        <Form
+            form={form}
+            layout={"vertical"}
+            name={"registration_form"}
+            className={"registration-form"}
+            onFinish={onFinish}
+            scrollToFirstError
+        >
+            <Form.Item
+                label={"Username"}
+                validateStatus={usernameValidationStatus}
+                help={usernameErrorMsg}
+                name={"username"}
+                rules={[
+                    {
+                        required: true
+                    },
+                    {
+                        validator: (rule, value) => {
+                            if (value) {
+                                setUsernameValidationStatus('success')
+                                setUsernameErrorMsg('')
+                                return Promise.resolve()
+                            } else {
+                                setUsernameValidationStatus('error')
+                                setUsernameErrorMsg('Username is required')
+                                return Promise.reject('')
+                            }
+                        }
+                    }
+                ]}
+            >
+                <Input
+                    value={username}
+                    onChange={event => setUsername(event.target.value)}
+                    onBlur={validateUsernameAvailability}/>
+            </Form.Item>
+
+            <Form.Item
+                label={"First Name"}
+                name={"firstName"}
+                rules={[
+                    {
+                        required: true,
+                        message: 'First Name is required'
+                    }
+                ]}
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item
+                label={"Last Name"}
+                name={"lastName"}
+                rules={[
+                    {
+                        required: true,
+                        message: 'Last Name is required'
+                    }
+                ]}
+            >
+                <Input/>
+            </Form.Item>
+
+            <Form.Item
+                label={"E-mail"}
+                validateStatus={emailValidationStatus}
+                help={emailErrorMsg}
+                name={"email"}
+                rules={[
+                    {
+                        required: true,
+                    },
+                    {
+                        validator: (rule, value) => {
+                            if (value) {
+                                setEmailValidationStatus('success')
+                                setEmailErrorMsg('')
+                                return Promise.resolve()
+                            } else {
+                                setEmailValidationStatus('error')
+                                setEmailErrorMsg('E-mail address is required')
+                                return Promise.reject('')
+                            }
+                        }
+                    }
+                ]}
+            >
+                <Input
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                    onBlur={validateEmailAvailability}/>
+            </Form.Item>
+
+            <Form.Item
+                label={"Password"}
+                name={"password"}
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: 'Password is required'
+                    }
+                ]}
+            >
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item
+                label={"Confirm Password"}
+                name={"confirm"}
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                    {
+                        required: true,
+                        message: 'Password confirmation is required'
+                    },
+                    ({getFieldValue}) => ({
+                        validator(rule, value) {
+                            if (!value || getFieldValue('password') === value) {
+                                return Promise.resolve()
+                            } else return Promise.reject('Passwords do not match');
+                        }
+                    })
+                ]}
+            >
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="registration-form-button">
+                    Register
+                </Button>
+                <text className="login-link">Already have an acoount? <a href="../login">Login</a></text>
+            </Form.Item>
+        </Form>
+    )
+}
 
 export default RegistrationForm
