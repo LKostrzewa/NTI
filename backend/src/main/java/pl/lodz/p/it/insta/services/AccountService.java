14e3478823d916c2674a3168357cf59177b43b63
216749 Krystian Domdalski <216749@edu.p.lodz.pl>
2020-06-15 21:39:48 +0200

    Backend update - services

--- backend/src/main/java/pl/lodz/p/it/insta/services/AccountService.java
+++ backend/src/main/java/pl/lodz/p/it/insta/services/AccountService.java
@@ -1,11 +1,11 @@
 package pl.lodz.p.it.insta.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import pl.lodz.p.it.insta.entities.Account;
+import pl.lodz.p.it.insta.exceptions.ResourceNotFoundException;
 import pl.lodz.p.it.insta.repositories.AccountRepository;
-
-import java.util.Optional;
 
 @Service
 public class AccountService {
@@ -20,8 +20,9 @@ public class AccountService {
         return accountRepository.save(account);
     }
 
-    public Optional<Account> findByUsername(String username) {
-        return accountRepository.findByUsername(username);
+    public Account findByUsername(String username) {
+        return accountRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Account", "username", username));
     }
 
     public Boolean existsByUsername(String login) {
@@ -30,5 +31,17 @@ public class AccountService {
 
     public Boolean existsByEmail(String email) {
         return accountRepository.existsByEmail(email);
+    }
+
+    public void editUser(String firstName, String lastName, String email) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Account editedAccount = accountRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("Account", "username", username));
+
+        editedAccount.setFirstName(firstName);
+        editedAccount.setLastName(lastName);
+        editedAccount.setEmail(email);
+
+        accountRepository.save(editedAccount);
     }
 }
